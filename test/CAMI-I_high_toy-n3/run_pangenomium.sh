0c020bc5b1d99466abeb38f227dbaaf2b966308c
Josh L. Espinoza <jol.espinoz@gmail.com>
2026-02-12 16:58:23 -0800

    Fixed run command

--- test/CAMI-I_high_toy-n3/run_pangenomium.sh
+++ test/CAMI-I_high_toy-n3/run_pangenomium.sh
@@ -1,1 +1,1 @@
-pangenomium end-to-end -i genomes_table.tsv -o results/pangenomium_output/ --n_threads_skani 12 --n_threads_mmseqs_per_task 12 
+pangenomium end-to-end -m veba -i genomes_table.tsv -o results/pangenomium_output/ --n_threads_skani 12 --n_threads_mmseqs_per_task 12 
